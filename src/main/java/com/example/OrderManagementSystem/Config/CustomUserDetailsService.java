a972d8963b81b92f400ea78b20f7d3669a20bded
JaganBi <1999biswajit@gmail.com>
2026-05-09 12:09:19 +0530

    JwtFilter changes

---
+++ src/main/java/com/example/OrderManagementSystem/Config/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.OrderManagementSystem.Config;
+
+import com.example.OrderManagementSystem.Entity.User;
+import com.example.OrderManagementSystem.Repository.UserRepository;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+      User user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+      return new CustomUserDetails(user);
+    }
+}
--- src/main/java/com/example/OrderManagementSystem/Filter/JwtFilter.java
+++ src/main/java/com/example/OrderManagementSystem/Filter/JwtFilter.java
@@ -1,5 +1,6 @@
 package com.example.OrderManagementSystem.Filter;
 
+import com.example.OrderManagementSystem.Config.CustomUserDetailsService;
 import com.example.OrderManagementSystem.Security.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,18 +8,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtFilter(
+            CustomUserDetailsService customUserDetailsService
+    ) {
+        this.customUserDetailsService =
+                customUserDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -34,12 +40,13 @@ public class JwtFilter extends OncePerRequestFilter {
             String token = authHeader.substring(7);
 
             String email = JwtUtil.extractEmail(token);
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
 
             UsernamePasswordAuthenticationToken authentication =
                     new UsernamePasswordAuthenticationToken(
-                            email,
+                            userDetails,
                             null,
-                            List.of(new SimpleGrantedAuthority("ROLE_USER"))
+                            userDetails.getAuthorities()
                     );
 
             authentication.setDetails(
